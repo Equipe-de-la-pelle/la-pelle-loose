160ca0e703737a72f6d7747bb322f1824dd76046
Alexandre Lairan <lairana@free.fr>
2018-10-17 16:16:49 +0200

    static headers

--- src/la-pelle-loose.cr
+++ src/la-pelle-loose.cr
@@ -4,6 +4,13 @@ module LaPelleLoose
   VERSION = "0.1.0"
 end
 
+static_headers do |response, filepath, filestat|
+  if filepath =~ /\.html$/
+    response.headers.add("Access-Control-Allow-Origin", "*")
+  end
+  response.headers.add("Content-Size", filestat.size.to_s)
+end
+
 error 404 do
   { error: :not_found }.to_json
 end
