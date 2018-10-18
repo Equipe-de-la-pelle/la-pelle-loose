f9d3db0011915ae4e9c4850ce95b39acee79d39e
Alexandre Lairan <lairana@free.fr>
2018-10-19 01:07:22 +0200

    Clean up

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -1,3 +1,11 @@
 require "spec"
 require "spec-kemal"
 require "../src/la-pelle-loose"
+
+Spec.before_each do
+  Models::Contact.migrator.drop_and_create
+  Models::Project.migrator.drop_and_create
+  Models::UserProject.migrator.drop_and_create
+  Models::User.migrator.drop_and_create
+  Models::Picture.migrator.drop_and_create
+end
