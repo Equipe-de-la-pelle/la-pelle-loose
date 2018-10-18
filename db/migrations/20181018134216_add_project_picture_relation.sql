fd72e0741a35f0ea1593709e10fe7de60f49366d
Cross Albeo <crossalbeo@yahoo.com>
2018-10-18 15:20:58 +0200

    ssssssnek

--- db/migrations/20181018134216_add_project_picture_relation.sql
+++ db/migrations/20181018134216_add_project_picture_relation.sql
@@ -3,7 +3,7 @@
 
 ALTER TABLE "projects"
 ADD COLUMN picture_id INTEGER NULL,
-ADD CONSTRAINT fk_project_picture FOREIGN KEY (picture_id) REFERENCES picture(id);
+ADD CONSTRAINT fk_project_picture FOREIGN KEY (picture_id) REFERENCES pictures(id);
 
 -- +micrate Down
 -- SQL section 'Down' is executed when this migration is rolled back
