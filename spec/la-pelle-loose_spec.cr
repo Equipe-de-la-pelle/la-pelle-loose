77029be7c695c885c0913f3234c0b69da5da901e
Alexandre Lairan <lairana@free.fr>
2018-10-18 22:56:26 +0200

    fix specs

--- spec/la-pelle-loose_spec.cr
+++ spec/la-pelle-loose_spec.cr
@@ -1,22 +1,11 @@
 require "./spec_helper"
-require "logger"
-
-logger = Logger.new(STDOUT,level: Logger::WARN)
 
 describe "La::Pelle::Loose" do
-  # TODO: Write tests
-
-  # it "works" do
-  #   false.should eq(true)
-  # end
-
   it "All Projects" do
-    get "/projects" do
-    end
+    get "/projects"
 
-    projects = JSON.parse(response.body.data)
+    projects = JSON.parse(response.body)["data"]
 
-    {projects.size > 15 && projects.size < 30}.should be_true
+    (projects.size > 15 && projects.size < 30).should be_true
   end
-
 end
