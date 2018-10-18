3aaa42d4aad9d081a8bbadfa87458ac8cd2e1e64
Cross Albeo <crossalbeo@yahoo.com>
2018-10-18 09:54:15 +0200

    Add function to user route

--- src/routes/users.cr
+++ src/routes/users.cr
@@ -17,10 +17,31 @@ get "/users/:id" do |env|
 end
 
 put "/users/:id" do |env|
+  nickname = env.params.json["nickname"].as(String)
+  first_name = env.params.json["first_name"].as(String)
+  last_name = env.params.json["last_name"].as(String)
 end
 
 delete "/users/:id" do |env|
+  user = Models::User.find(env.params.url["id"])
+  if user
+    user.destroy
+  else
+    raise Kemal::Exceptions::RouteNotFound.new(env)
+  end
 end
 
 post "/users/:id/picture" do |env|
+  HTTP::FormData.parse(env.request) do |upload|
+      filename = upload.filename
+    # Be sure to check if file.filename is not empty otherwise it'll raise a compile time error
+      if !filename.is_a?(String)
+        p "No filename included in upload"
+      else
+        file_path = ::File.join [Kemal.config.public_folder, "uploads/users", filename]
+        File.open(file_path, "w") do |f|
+          IO.copy(upload.body, f)
+        end
+      end
+    end
 end
