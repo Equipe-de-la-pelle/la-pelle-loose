d3fa58a3531f411808702a628dcc132b52dc128e
Florian <f.thery3103@gmail.com>
2018-10-18 22:47:44 +0200

    debut spec

--- spec/la-pelle-loose_spec.cr
+++ spec/la-pelle-loose_spec.cr
@@ -1,11 +1,21 @@
 require "./spec_helper"
+require "logger"
+
+logger = Logger.new(STDOUT,level: Logger::WARN)
 
 describe "La::Pelle::Loose" do
+  # TODO: Write tests
+
+  # it "works" do
+  #   false.should eq(true)
+  # end
+
   it "All Projects" do
-    get "/projects"
+    get "/projects" do
+    end
 
-    projects = JSON.parse(response.body)["data"]
+    projects = JSON.parse(response.body.data)
 
-    (projects.size > 15 && projects.size < 30).should be_true
+    {projects.size > 15 && projects.size < 30}.should be_true
   end
 end
