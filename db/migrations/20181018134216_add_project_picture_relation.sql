7cf37306f7e149e23aef821eacc049e168feaa03
Cross Albeo <crossalbeo@yahoo.com>
2018-10-18 15:10:00 +0200

    project picture

---
+++ db/migrations/20181018134216_add_project_picture_relation.sql
@@ -0,0 +1,9 @@
+-- +micrate Up
+-- SQL in section 'Up' is executed when this migration is applied
+
+ALTER TABLE "projects"
+ADD COLUMN picture_id INTEGER NULL,
+ADD CONSTRAINT fk_project_picture FOREIGN KEY (picture_id) REFERENCES picture(id);
+
+-- +micrate Down
+-- SQL section 'Down' is executed when this migration is rolled back
--- src/routes/projects.cr
+++ src/routes/projects.cr
@@ -48,4 +48,60 @@ delete "/projects/:id" do |env|
 end
 
 post "/projects/:id/picture" do |env|
+  project = Models::Project.find(env.params.url["id"])
+
+  if project
+    HTTP::FormData.parse(env.request) do |upload|
+      filename = upload.filename
+
+      if !filename.is_a?(String)
+        { error: "Filename is missing" }.to_json
+      else
+        file_path = ::File.join ["uploads/projects", filename]
+
+        picture = Models::Picture.new
+        picture.name = filename
+        picture.path = file_path
+
+        picture.save
+
+        pp project.picture
+
+        if project_picture = project.picture
+          pp project_picture
+          File.delete(picture.path.not_nil!)
+          pp project_picture.destroy
+        end
+
+        project.picture_id = picture.id
+        pp project.save
+
+        File.open(file_path, "w") do |f|
+          IO.copy(upload.body, f)
+        end
+
+        { upload: :ok }.to_json
+      end
+    end
+  else
+    raise Kemal::Exceptions::RouteNotFound.new(env)
+  end
+end
+
+get "projects/:id/picture" do |env|
+  project = Models::Project.find(env.params.url["id"])
+  if project
+    picture = project.picture
+
+    pp picture
+
+    if picture
+      file_path = ::File.join ["uploads/projects", picture.path.not_nil!]
+      send_file env, file_path
+    else
+      { error: :error }.to_json
+    end
+  else
+    raise Kemal::Exceptions::RouteNotFound.new(env)
+  end
 end
