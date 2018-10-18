1f874b665fabebea5b9eb6aced811910913c2f0a
Florian <f.thery3103@gmail.com>
2018-10-19 00:37:03 +0200

    Project TU

--- spec/la-pelle-loose_spec.cr
+++ spec/la-pelle-loose_spec.cr
@@ -6,6 +6,44 @@ describe "La::Pelle::Loose" do
 
     projects = JSON.parse(response.body)["data"]
 
-    (projects.size > 15 && projects.size < 30).should be_true
+    (projects.size > 45 && projects.size < 55).should be_true
   end
+
+    it "Find project #53" do
+      get "/projects/53"
+
+      project = JSON.parse(response.body)
+      if project["error"] == nil
+        project["data"]["id"].should eq 53
+        project["data"]["title"].should eq "Un projet magnifique XXX :)"
+        project["data"]["description"].should eq "Lorem ipsim lasum danem"
+        project["data"]["short"].should eq "Lorem ipsum"
+        project["data"]["email"].should eq ""
+      else
+        project["error"].should eq "not_found"
+      end
+    end
+
+    it "new project #53" do
+      post "/projects" do |env|
+        env.params.json.to_json
+      end
+
+      json_body = {	"title"=>"Un projet magnifique","description"=>"Lorem ipsim lasum danem","short"=>"Lorem ipsum","email"=>""}
+      post("projects",headers: HTTP::Headers{"Content-Type"=>"application/json"}, body: json_body.to_json)
+
+      project = JSON.parse(response.body)["data"]
+      project["title"].should eq "Un projet magnifique"
+      project["description"].should eq "Lorem ipsim lasum danem"
+      project["short"].should eq "Lorem ipsum"
+      project["email"].should eq ""
+
+
+    end
+
+
+    it "Delete #53" do
+      delete "/projects/53"
+      (response.status_code == 200 || response.body["error"] != nil).should be_true
+    end
 end
